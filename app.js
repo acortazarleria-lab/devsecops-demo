d6e505a3a432d69b4cb9111b44b3ee7bd28f3528
Rafa Sánchez <rafasanchez@MacBook-Air-de-Rafa.local>
2026-06-20 12:35:48 +0200

    Nueva versión vulnerable del sábado...

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-function escapeHtml(unsafe) {
-  return unsafe
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
 
 app.get('/', (req, res) => {
   const name = req.query.name || 'World';
-  // ✅ Corregido: sanitizamos la entrada
-  res.send(`<h1>Hello, ${escapeHtml(name)}!</h1>`);
+  // Vuelve a ser vulnerable...
+  res.send(`<h1>Hello, ${name}!</h1>`);
 });
 
 app.listen(port, () => {
